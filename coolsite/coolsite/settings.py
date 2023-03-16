911ec2ad460853613311c42f5a4c2890a97c42b0
Ilya131997 <ilya.yakim131997@gmail.com>
2023-03-17 00:08:03 +0300

    3 lessons

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*3#!z27y+@19p+slqo$peu0y#+#u%$x!ud=)((s5hp2+a$a7)9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
